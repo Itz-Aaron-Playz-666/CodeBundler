30e290776ad12d38e5af98f16b6da0370e6125eb
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-03-01 02:09:14 +0000

    Added Stuffs

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import CodeFormatter from "../script";
+CodeFormatter.displayName = "CodeFormatter";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome To CodeBundler!</h1>
+      <CodeFormatter />
+    </main>
+  );
+}
